da4141b58cf667ef7e0e03be5c41ad829643f8ff
Bryce Bixler <bbixler@ucsd.edu>
2023-09-25 11:13:30 -0700

    Changed record process to partition data into smaller files

--- testing/record_chwp_encoder_data.py
+++ testing/record_chwp_encoder_data.py
@@ -2,15 +2,22 @@ from spt3g import core  # pylint: disable=import-error
 from spt3g import chwp # pylint: disable=import-error
 import time
 import argparse
+from datetime import datetime
 
 
 # generate bolometer timepoint frames artificially
 num_frames = 0
 
+db_path = ''
 
 def timed_read(frame, start_time, time_len, samp_rate=10):
     global num_frames
-    while time.time() < start_time + time_len:
+
+    while datetime.utcnow().timestamp() < start_time + time_len:
+        cur_day = datetime.utcnow().strftime("%Y%m%d")
+        if not os.path.isdir(os.path.join(db_path, cur_day)):
+            os.mkdir(os.path.join(db_path, cur_day))
+
         if num_frames % (10 * samp_rate) == 0:
             print("%d secs remaining..."
                   % (int(time_len-(num_frames / samp_rate))))
@@ -25,14 +32,19 @@ def timed_read(frame, start_time, time_len, samp_rate=10):
     return []
 
 
+def add_datetime(frame):
+    if (frame.type == core.G3FrmaeType.Timepoint and
+            ('chwp_encoder_clock' in frame.keys() or
+            'chwp_irig_clock' in frame.keys())):
+        now = datetime.utcnow()
+        frame['frame_day'] = now.strftime("%Y%m%d")
+        frame['frame_time'] = now.strftime("%Y%m%d%H%M%S")
+
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '--time', type=int, help='Number of seconds to record for',
     default=10)
-parser.add_argument(
-    '--out_file', type=str, help='Save data to output file with this path',
-    default=None)
 parser.add_argument(
     '--port', type=int, help='Port to collect packets from',
     default=8080)
@@ -42,16 +54,19 @@ args = parser.parse_args()
 # Connect to G3 pipeline
 pipe = core.G3Pipeline()
 # Add module to generate dummy MUX data
-pipe.Add(timed_read, start_time=time.time(), time_len=args.time)
+pipe.Add(timed_read, start_time=datetime.utcnow().timestamp(), time_len=args.time)
 # Start the collection of packets from the CHWP MCU
 chwp_collector = chwp.CHWPCollector(mcu_port=args.port)
 # Insert CHWP data into dummy MUX frames in the pipeline
 pipe.Add(chwp.CHWPBuilder, collector=chwp_collector)
 # Send CHWP data out for slow DAQ publishing
 #pipe.Add(chwp.CHWPSlowDAQTee)
+
+pipe.Add(add_datetime)
 # Write data to a G3 file
 if args.out_file is not None:
-    pipe.Add(core.G3Writer, filename=args.out_file)
+    pipe.Add(core.G3MultiFileWriter, filename=lambda fr: os.path.join(db_path,fr['frame_day'],fr['frame_time']),
+             size_limit=2**20)
 
 # Run the pipeline
 pipe.Run(profile=True)
